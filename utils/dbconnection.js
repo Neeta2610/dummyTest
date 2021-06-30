b50853a626afe9d7c32c56a1bc55dea7b8c09559
Shriom Tyagi <shriom.tyagi@vernost.in>
2021-06-30 12:30:03 +0530

    some changes

--- utils/dbconnection.js
+++ utils/dbconnection.js
@@ -20,7 +20,7 @@ let _db;
 
 const mongoConnect = callback => {
     
-    var dbName ="mongodb+srv://Shriom:Sh123456@cluster0.8hhfw.mongodb.net/Cluster0?retryWrites=true&w=majority"
+    var dbName ="mongodb+srv://Airport:0D80aY6DwSmdJCaN@cluster0.8hhfw.mongodb.net/Cluster0?retryWrites=true&w=majority"
     MongoClient.connect(
         dbName, dbOptions
     )
